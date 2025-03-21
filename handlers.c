973a5e3e1e8193cc220558ca3a859615cc2ad491
Dmitry <switchnight@mail.ru>
2025-03-21 18:48:40 +0500

    Remove scroll bug

--- handlers.c
+++ handlers.c
@@ -183,8 +183,6 @@ void write_to_address (struct NESEmu *emu, uint16_t addr, uint8_t *r)
 			uint32_t indx = emu->scanline / 8;
 			emu->scroll_x[emu->max_scroll_indx] = *r;
 			uint32_t res_indx = indx / 8;
-			if (res_indx == 3)
-				res_indx = 4;
 			emu->scroll_tile_x[emu->max_scroll_indx] = res_indx;
 			emu->max_scroll_indx++;
 			//printf ("scroll x: %02x from %04x; indx: %d; linetile: %d\n", *r, emu->cpu.PC, indx, indx / 8);
